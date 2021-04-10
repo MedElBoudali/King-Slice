6de58e813cf20c842db08ce20032d4c65451c09c
MedElBoudali <med.sneaky@gmail.com>
2021-04-10 22:13:00 +0000

    🎨 added logo

---
+++ src/components/common/Logo.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import stripes from "../../assets/images/stripes.svg";
+
+const LogoStyles = styled.div`
+  /* This value controls the entire size of the logo*/
+  ${({ logoStyle }) => logoStyle}
+  font-size: 6px;
+  font-size: clamp(1px, 0.65vw, 8px);
+  width: 30em;
+  height: 30em;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
+  margin: 0;
+  --borderSize: 1em;
+  background: white url(${stripes});
+  background-size: 150em;
+  border: var(--borderSize) solid white;
+  display: flex;
+  div {
+    margin: var(--borderSize);
+    flex: 1;
+    background: white;
+    display: grid;
+    grid-template-rows: 20% 1fr 1fr;
+    align-content: center;
+  }
+  h1 {
+    display: grid;
+    grid-template-rows: 8fr 2fr;
+    align-items: center;
+    margin: 0;
+    grid-row: 2 / span 2;
+    grid-gap: 2em;
+    transform: translateY(-0.7em);
+  }
+`;
+
+const Est = styled.span`
+  font-size: 1.5em;
+  align-self: center;
+`;
+
+const LettersWrapper = styled.span`
+  transform: scale(1.4);
+  display: block;
+  text-shadow: 0.18em 0.18em 0 rgba(0, 0, 0, 0.05);
+  perspective: 100px;
+`;
+
+const Letter = css`
+  font-size: 5em;
+  color: var(--red);
+  --scale: 1;
+  --rotate: -10deg;
+  --translateX: 0;
+  --translateY: 0;
+  --rotateX: 0deg;
+  transform: scale(var(--scale)) rotate(var(--rotate))
+    translateX(var(--translateX)) translateY(var(--translateY))
+    rotateX(var(--rotateX));
+  display: inline-block;
+  line-height: 1;
+  transition: transform 0.3s;
+`;
+
+const KLetter = styled.span`
+  ${Letter}
+  --translateX: -0.05;
+`;
+const ILetter = styled.span`
+  ${Letter}
+  --rotate: 5deg;
+  --scale: 1.1;
+  --translateX: 0.05em;
+  --translateY: 0.2em;
+`;
+const NLetter = styled.span`
+  ${Letter}
+  --scale: 0.9;
+  --translateY: -0.1em;
+  --translateX: 0.1em;
+`;
+const GLetter = styled.span`
+  ${Letter}
+  --rotate: 3deg;
+  --scale: 0.9;
+  --translateX: 0.1em;
+  --translateY: 0.23em;
+`;
+
+const Slice = styled.span`
+  font-size: 3.2em;
+  letter-spacing: 0.2em;
+  transform: translateY(-0.15em);
+`;
+
+export default function Logo({ logoStyle }) {
+  return (
+    <LogoStyles logoStyle={logoStyle}>
+      <div>
+        <Est>EST 1994</Est>
+        <h1>
+          <LettersWrapper>
+            <KLetter>k</KLetter>
+            <ILetter>i</ILetter>
+            <NLetter>n</NLetter>
+            <GLetter>g</GLetter>
+          </LettersWrapper>
+          <Slice>slice</Slice>
+        </h1>
+      </div>
+    </LogoStyles>
+  );
+}
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,14 +1,54 @@
 import React from "react";
 import { Link } from "gatsby";
+import styled, { css } from "styled-components";
+import Logo from "./Logo";
+
+const NavStyle = styled.nav`
+  display: grid;
+  grid-template-columns: 1fr 1fr auto 1fr 1fr;
+  grid-gap: 2rem;
+  margin-bottom: 3rem;
+  align-items: center;
+  text-align: center;
+`;
+
+const NavStyleLink = styled(Link)`
+  --rotate: 0deg;
+  transform: rotate(var(--rotate));
+  font-size: 3rem;
+
+  &:nth-child(1),
+  &:nth-child(4) {
+    --rotate: 6deg;
+  }
+  &:nth-child(2),
+  &:nth-child(5) {
+    --rotate: -6deg;
+  }
+  &:not(:last-child) {
+    /* margin-right: 10px; */
+  }
+
+  &:hover {
+    color: var(--red);
+    --rotate: -1deg;
+  }
+`;
+
+const LogoStyle = css`
+  /* transform: translateY(-25%); */
+`;
 
 const Navbar = () => (
-  <nav>
-    <Link to="/">hot now</Link>
-    <Link to="/pizzas">pizza menu</Link>
-    <Link to="/">logo</Link>
-    <Link to="/slicekings">slicekings</Link>
-    <Link to="/order">order ahead</Link>
-  </nav>
+  <NavStyle>
+    <NavStyleLink to="/">hot now</NavStyleLink>
+    <NavStyleLink to="/pizzas">pizza menu</NavStyleLink>
+    <NavStyleLink to="/">
+      <Logo logoStyle={LogoStyle} />
+    </NavStyleLink>
+    <NavStyleLink to="/slicekings">slicekings</NavStyleLink>
+    <NavStyleLink to="/order">order ahead</NavStyleLink>
+  </NavStyle>
 );
 
 export default Navbar;
