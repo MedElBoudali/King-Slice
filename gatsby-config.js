e30690b1259e7f1487920664a5ea7a6f1fcff2cc
MedElBoudali <med.sneaky@gmail.com>
2021-04-10 20:50:23 +0000

    🎨 added styles files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,40 @@
+// import dotenv from "dotenv";
+// dotenv.config({ path: ".env" });
 // module.exports = {
+//   pathPrefix: "/pizza",
 //   siteMetadata: {
-//     title: `Gatsby Default Starter`,
-//     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-//     author: `@gatsbyjs`,
+//     title: `Slicks Slices`,
+//     siteUrl: "https://gatsby.pizza",
+//     description: "The best pizza place in Hamilton!",
+//     twitter: "@slicksSlices",
 //   },
 //   plugins: [
-//     `gatsby-plugin-react-helmet`,
-//     `gatsby-plugin-image`,
-//     {
-//       resolve: `gatsby-source-filesystem`,
-//       options: {
-//         name: `images`,
-//         path: `${__dirname}/src/images`,
-//       },
-//     },
-//     `gatsby-transformer-sharp`,
-//     `gatsby-plugin-sharp`,
-//     {
-//       resolve: `gatsby-plugin-manifest`,
-//       options: {
-//         name: `gatsby-starter-default`,
-//         short_name: `starter`,
-//         start_url: `/`,
-//         background_color: `#663399`,
-//         theme_color: `#663399`,
-//         display: `minimal-ui`,
-//         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-//       },
-//     },
-//     `gatsby-plugin-gatsby-cloud`,
-//     // this (optional) plugin enables Progressive Web App + Offline functionality
-//     // To learn more, visit: https://gatsby.dev/offline
-//     // `gatsby-plugin-offline`,
+//     "gatsby-plugin-react-helmet",
+//     "gatsby-plugin-styled-components",
+//     "gatsby-plugin-image",
+//     // {
+//     //   // this is the name of the plugin you are adding
+//     //   resolve: "gatsby-source-sanity",
+//     //   options: {
+//     //     projectId: "0jfvvkkd",
+//     //     dataset: "production",
+//     //     watchMode: true,
+//     //     token: process.env.SANITY_TOKEN,
+//     //   },
+//     // },
 //   ],
-// }
+// };
+
+module.exports = {
+  siteMetadata: {
+    title: `King Slices`,
+    description: `The best pizza place in Hamilton!`,
+    siteUrl: "https://gatsby.pizza",
+    author: `@KingSlices`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-styled-components",
+    `gatsby-plugin-image`,
+  ],
+};
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import Navbar from "./common/Navbar";
 import PropTypes from "prop-types";
 import Footer from "./common/Footer";
+import GlobalStyles from "../styles/GlobalStyles";
+import "normalize.css";
+import Typography from "../styles/Typography";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyles />
+      <Typography />
       <header>
         <Navbar />
       </header>
