47963e932969b54b60868aba4dceeb12328315a7
MedElBoudali <med.sneaky@gmail.com>
2021-04-17 22:27:14 +0000

    🎨 added grid to pizzalist

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import styled from "styled-components";
 import PropTypes from "prop-types";
+
+const PizzaWrapper = styled.div`
+  display: grid;
+  /* Take style from PizzaListWrapper not from PizzaWrapper */
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 300px;
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 const Pizza = ({ pizza }) => {
   return (
-    <div>
+    <PizzaWrapper>
       <Link to={`/pizza/${pizza.slug.current}`}>
         <h2>
           <span className="mark">{pizza.name}</span>
         </h2>
-        <p>{pizza.toppings.map(topping => topping.name).join(", ")}</p>
       </Link>
+      <p>{pizza.toppings.map(topping => topping.name).join(", ")}</p>
       {pizza.image && (
         <GatsbyImage image={getImage(pizza.image.asset)} alt={pizza.name} />
       )}
-    </div>
+    </PizzaWrapper>
   );
 };
 
--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Pizza from "./Pizza";
+import styled from "styled-components";
 import PropTypes from "prop-types";
+
+const PizzaListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 300px;
+`;
 
 const PizzaList = ({ pizzas }) => {
   return (
-    <>
+    <PizzaListWrapper>
       {pizzas.map(pizza => (
         <Pizza pizza={pizza} key={pizza.id} />
       ))}
-    </>
+    </PizzaListWrapper>
   );
 };
 
