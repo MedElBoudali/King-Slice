4f5a78cfc44ee967882af147d6fcb5754ddee919
MedElBoudali <med.sneaky@gmail.com>
2021-04-26 23:22:16 +0000

    🎨 added useForm hook

--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -8,7 +8,21 @@ const Order = ({ location }) => (
       description="Order Pizza."
       location={location.href}
     />
-    <h1>Hello! i'm Order page</h1>
+    <form>
+      <fieldset>
+        <legend>Your Info</legend>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" />
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" />
+      </fieldset>
+      <fieldset>
+        <legend>MENU</legend>
+      </fieldset>
+      <fieldset>
+        <legend>ORDER</legend>
+      </fieldset>
+    </form>
   </>
 );
 
