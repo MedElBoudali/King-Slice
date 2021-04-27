f9c4a5f588e9ac2a6d7d9855382f38d90840a1a6
MedElBoudali <med.sneaky@gmail.com>
2021-04-27 23:29:30 +0000

    🎨 styling done

--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -5,6 +5,60 @@ import useForm from "../hooks/useForm";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import calculatePizzaPrice from "../utils/calculatePizzaPrice";
 import formatMoney from "../utils/formatMoney";
+import styled from "styled-components";
+
+const OrderForm = styled.form`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+  fieldset {
+    grid-column: span 2;
+    max-height: 600px;
+    display: grid;
+    gap: 1rem;
+    overflow: auto;
+    &:nth-child(2),
+    &:nth-child(3) {
+      grid-column: span 2;
+      @media (min-width: 800px) {
+        grid-column: span 1;
+      }
+    }
+  }
+`;
+
+const MenuItem = styled.div`
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  grid-template-rows: repeat(2, 1fr);
+  gap: 0 1.3rem;
+  align-content: center;
+  align-items: center;
+  .gatsby-image-wrapper {
+    grid-row: span 2;
+    height: 100px;
+  }
+  p {
+    margin: 0;
+  }
+  button {
+    font-size: 1.4rem;
+  }
+  button + button {
+    margin-left: 1rem;
+  }
+
+  .remove {
+    background: none;
+    color: var(--red);
+    font-size: 3rem;
+    position: absolute;
+    top: 0;
+    right: 0;
+    box-shadow: none;
+    line-height: 1rem;
+  }
+`;
 
 const Order = ({
   data: {
@@ -24,7 +78,7 @@ const Order = ({
         description="Order Pizza."
         location={location.href}
       />
-      <form>
+      <OrderForm>
         <fieldset>
           <legend>Your Info</legend>
           <label htmlFor="name">Name</label>
@@ -45,15 +99,13 @@ const Order = ({
         <fieldset>
           <legend>MENU</legend>
           {pizzas.map(({ id, name, price, image, slug: { current } }) => (
-            <div key={id}>
+            <MenuItem key={id}>
               <GatsbyImage
                 image={getImage(image ? image.asset : "")}
                 alt={name}
               />
               <div>
-                <h2>
-                  {name} + {id}
-                </h2>
+                <h2>{name}</h2>
               </div>
               <div>
                 {["S", "M", "L"].map((size, id) => (
@@ -62,13 +114,13 @@ const Order = ({
                   </button>
                 ))}
               </div>
-            </div>
+            </MenuItem>
           ))}
         </fieldset>
         <fieldset>
           <legend>ORDER</legend>
         </fieldset>
-      </form>
+      </OrderForm>
     </>
   );
 };
