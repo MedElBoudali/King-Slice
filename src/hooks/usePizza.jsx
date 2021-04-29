cb6d2ffc44e89371c60a672806f48c0ec2f073e5
MedElBoudali <med.sneaky@gmail.com>
2021-04-29 15:35:10 +0000

    🎨 addded submitOrder function

--- src/hooks/usePizza.jsx
+++ src/hooks/usePizza.jsx
@@ -1,19 +1,42 @@
 import { useContext, useState } from "react";
+import attachNamesAndPrices from "../utils/attachNamesAndPrices";
+import calculateOrderTotal from "../utils/calculateOrderTotal";
+import formatMoney from "../utils/formatMoney";
 import OrderContext from "./context/OrderContext";
 
-const usePizza = ({ pizzas, inputs }) => {
+const usePizza = ({ pizzas, values: { name, email } }) => {
   // before using useContext
   // const [orders, setOrders] = useState([]);
   const [orders, setOrders] = useContext(OrderContext);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+
   const addOrder = orderedPizza => setOrders([...orders, orderedPizza]);
 
   const removeOrder = index =>
     setOrders([...orders.slice(0, index), ...orders.slice(index + 1)]);
 
+  const submitOrder = e => {
+    e.preventDefault();
+    setLoading(true);
+    const body = {
+      orders: attachNamesAndPrices(orders, pizzas),
+      total: formatMoney(calculateOrderTotal(orders, pizzas)),
+      name,
+      email,
+    };
+    console.log(body);
+  };
+
   return {
     orders,
     addOrder,
     removeOrder,
+    loading,
+    error,
+    message,
+    submitOrder,
   };
 };
 
