b3b3083b20adca63975c73b03f029af5524b99a0
MedElBoudali <med.sneaky@gmail.com>
2021-04-29 16:16:10 +0000

    ✨ added fetching with error handling

--- src/hooks/usePizza.jsx
+++ src/hooks/usePizza.jsx
@@ -17,16 +17,39 @@ const usePizza = ({ pizzas, values: { name, email } }) => {
   const removeOrder = index =>
     setOrders([...orders.slice(0, index), ...orders.slice(index + 1)]);
 
-  const submitOrder = e => {
+  const submitOrder = async e => {
     e.preventDefault();
     setLoading(true);
+    setError("");
+    setMessage("");
+
     const body = {
       orders: attachNamesAndPrices(orders, pizzas),
       total: formatMoney(calculateOrderTotal(orders, pizzas)),
       name,
       email,
     };
-    console.log(body);
+
+    const res = await fetch(
+      `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "appliction/json",
+          body: JSON.stringify(body),
+        },
+      }
+    );
+
+    const text = JSON.parse(await res.text());
+
+    if (res.status >= 400 && res.status < 600) {
+      setLoading(false);
+      setError(text.message);
+    } else {
+      setLoading(false);
+      setMessage("Success! Come on down for your pizzas");
+    }
   };
 
   return {
