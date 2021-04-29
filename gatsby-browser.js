0783670e65adbbb9257b9de080c5eeb564517225
MedElBoudali <med.sneaky@gmail.com>
2021-04-29 00:13:26 +0000

    🎨 added wrapRootElement with OrderProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Layout from "./src/components/layout/Layout";
+import { OrderProvider } from "./src/hooks/context/OrderContext";
 
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
+);
+
+export const wrapRootElement = ({ element }) => (
+  <OrderProvider>{element}</OrderProvider>
 );
