46579cc59fccb5c35482a51df43d95b36c28b799
MedElBoudali <med.sneaky@gmail.com>
2021-05-03 17:36:49 +0000

    🎨 added hotSlices and sliceKings

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,53 @@
 import React from "react";
 import Seo from "../components/common/Seo";
 import styled from "styled-components";
+import useLatestData from "../hooks/useLatestData";
+import LoadingPlaceholder from "../components/LoadingPlaceholder";
 
-const CurrentlySlicing = styled.div``;
+const HomeWrapper = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(2, minmax(auto, 1fr));
+`;
+
+const CurrentlySlicingWrapper = styled.div``;
 
 const HotSlicesWrapper = styled.div``;
 
-const HotSlices = () => {
-  return (
-    <HotSlicesWrapper>
-      <p>HotSlices</p>
-    </HotSlicesWrapper>
-  );
-};
-
 const Home = ({ location }) => {
+  const { hotSlices, sliceKings } = useLatestData();
+
   return (
     <div className="center">
       <Seo title="HOT NOW" location={location.href} />
       <h1>The Best Pizza Downtown!</h1>
-      <p>Open 11am to 11pm Every Single Day.</p>
-      <CurrentlySlicing></CurrentlySlicing>
-      <HotSlices />
+      <p>
+        <span role="img" aria-label="Clock">
+          🕚
+        </span>{" "}
+        Open 11am to 11pm Every Single Day.
+      </p>
+      <HomeWrapper>
+        <CurrentlySlicing sliceKings={sliceKings} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomeWrapper>
     </div>
+  );
+};
+
+const CurrentlySlicing = () => {
+  return (
+    <CurrentlySlicingWrapper>
+      <LoadingPlaceholder count={3} />
+    </CurrentlySlicingWrapper>
+  );
+};
+
+const HotSlices = () => {
+  return (
+    <HotSlicesWrapper>
+      <LoadingPlaceholder count={4} />
+    </HotSlicesWrapper>
   );
 };
 
