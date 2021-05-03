4a93811e347ec7573b96a4c2663ea36694684275
MedElBoudali <med.sneaky@gmail.com>
2021-05-03 00:22:37 +0000

    🎨 added useLatestData hook

---
+++ src/hooks/useLatestData.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const useLatestData = () => {
+  const [hotSlices, setHotSlices] = useState();
+  return <div></div>;
+};
+
+export default useLatestData;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Seo from "../components/common/Seo";
+import styled from "styled-components";
+
+const CurrentlySlicing = styled.div``;
+
+const HotSlicesWrapper = styled.div``;
+
+const HotSlices = () => {
+  return (
+    <HotSlicesWrapper>
+      <p>HotSlices</p>
+    </HotSlicesWrapper>
+  );
+};
 
 const Home = ({ location }) => {
   return (
-    <>
+    <div className="center">
       <Seo title="HOT NOW" location={location.href} />
-      <h1>Hello! i'm home page</h1>
-    </>
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day.</p>
+      <CurrentlySlicing></CurrentlySlicing>
+      <HotSlices />
+    </div>
   );
 };
 
