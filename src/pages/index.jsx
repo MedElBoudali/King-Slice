3f6bf7df014aa5e3c67108c24ae9ad520967934c
MedElBoudali <med.sneaky@gmail.com>
2021-05-04 01:44:27 +0000

    ✨ added CuttentlySlicing and HotSlices components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,52 +2,48 @@ import React from "react";
 import Seo from "../components/common/Seo";
 import styled from "styled-components";
 import useLatestData from "../hooks/useLatestData";
-import LoadingPlaceholder from "../components/LoadingPlaceholder";
+import Skeleton from "../components/Skeleton";
 
 const HomeWrapper = styled.div`
   display: grid;
   gap: 2rem;
   grid-template-columns: repeat(2, minmax(auto, 1fr));
+  padding: 2rem 0;
 `;
-
-const CurrentlySlicingWrapper = styled.div``;
-
-const HotSlicesWrapper = styled.div``;
 
 const Home = ({ location }) => {
   const { hotSlices, sliceKings } = useLatestData();
 
   return (
-    <div className="center">
+    <>
       <Seo title="HOT NOW" location={location.href} />
-      <h1>The Best Pizza Downtown!</h1>
-      <p>
-        <span role="img" aria-label="Clock">
-          🕚
-        </span>{" "}
-        Open 11am to 11pm Every Single Day.
-      </p>
-      <HomeWrapper>
-        <CurrentlySlicing sliceKings={sliceKings} />
-        <HotSlices hotSlices={hotSlices} />
-      </HomeWrapper>
+      <div className="center">
+        <h1>The Best Pizza Downtown!</h1>
+        <p>Open 11am to 11pm Every Single Day.</p>
+        <HomeWrapper>
+          <CurrentlySlicing sliceKings={sliceKings} />
+          <HotSlices hotSlices={hotSlices} />
+        </HomeWrapper>
+      </div>
+    </>
+  );
+};
+
+const CurrentlySlicing = ({ sliceKings }) => {
+  return (
+    <div>
+      {!sliceKings && <Skeleton count={4} />}
+      {sliceKings && !sliceKings?.length && <p>No one is working right now!</p>}
     </div>
   );
 };
 
-const CurrentlySlicing = () => {
+const HotSlices = ({ hotSlices }) => {
   return (
-    <CurrentlySlicingWrapper>
-      <LoadingPlaceholder count={3} />
-    </CurrentlySlicingWrapper>
-  );
-};
-
-const HotSlices = () => {
-  return (
-    <HotSlicesWrapper>
-      <LoadingPlaceholder count={4} />
-    </HotSlicesWrapper>
+    <div>
+      {!hotSlices && <Skeleton count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothin'g in the Case!</p>}
+    </div>
   );
 };
 
